8164a06580f56d6e15238c4344ee1073ce2f57ff
Orlando Caraballo <orlandocaraballo@gmail.com>
2018-03-18 20:47:57 -0400

    Removed some code

--- 11-behavioral-js/main.js
+++ 11-behavioral-js/main.js
@@ -1,14 +1,10 @@
-document.addEventListener("DOMContentLoaded", element => {
+document.addEventListener("DOMContentLoaded", (event) => {
   let ul = document.getElementsByTagName("ul")[0]
   let children = ul.children
 
   for(let i = 0; i < children.length; ++i) {
-    let _this = this
-
-    children[i].addEventListener("click", (event) => {
-      alert(event.target.innerHTML)
+    children[i].addEventListener("click", (clickEvent) => {
+      alert(clickEvent.target.innerHTML)
     })
   }
-
-  console.log(ul)
 })

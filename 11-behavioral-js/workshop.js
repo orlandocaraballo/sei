d8a888757847a144c31ec59ae293158ab02df3b2
orlandocaraballo <orlando.caraballo@nycda.com>
2018-03-19 19:50:12 -0400

    Added some comments to workshop code

--- 11-behavioral-js/workshop.js
+++ 11-behavioral-js/workshop.js
@@ -1,25 +1,28 @@
+// set your variables for later use
 let sections = document.getElementsByTagName("section")
 let firstSection = sections[0]
 let secondSection = sections[1]
 let thirdSection = sections[2]
 let lastSection = sections[3]
 
-
+// run this code on window scroll
 window.addEventListener("scroll", event => {
-  let section = document.querySelectorAll("section")
-  
+  // when the page scrolls 100px down then do the following
   if(window.scrollY > 100) {
     firstSection.classList.add("fadeIn")
   }
   
+  // when the page scrolls 800px down then do the following
   if(window.scrollY > 800) {
     secondSection.classList.add("fadeIn")
   }
 
+  // when the page scrolls 1600px down then do the following
   if(window.scrollY > 1600) {
     thirdSection.classList.add("fadeIn")
   }
 
+  // when the page scrolls 2400px down then do the following
   if(window.scrollY > 2400) {
     lastSection.classList.add("fadeIn")
   }

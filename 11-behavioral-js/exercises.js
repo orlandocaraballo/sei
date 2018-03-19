3be77fa743034152becb942991af221ba7219e67
orlandocaraballo <orlando.caraballo@nycda.com>
2018-03-19 14:59:20 -0400

    Changed some other things

--- 11-behavioral-js/exercises.js
+++ 11-behavioral-js/exercises.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  let title = document.getElementById("nycda-title")
-  let helpMessage = document.getElementById("nycda-help-message")
-  let lastNycdaClass = document.getElementsByClassName("nycda-class")[3]
-  let paragraphs = document.getElementsByTagName("p")
-  let redBorder = document.querySelector("input")
-
-  title.innerHTML = "<em>Welcome to NYCDA!</em>"
-  helpMessage.innerText = "Ask someone at NYCDA for help!"
+  let submitButton = document.getElementById("submit-button")
+  let textField = document.getElementById("input-important")
   
-  redBorder.style.borderColor = "red"
+  submitButton.addEventListener("click", (event) => {
+    alert( textField.value )
+  })
 })
+
+
+
+
+
+
+
+
+// let title = document.getElementById("nycda-title")
+// let helpMessage = document.getElementById("nycda-help-message")
+// let lastNycdaClass = document.getElementsByClassName("nycda-class")[3]
+// let paragraphs = document.getElementsByTagName("p")
+// let redBorder = document.querySelector("input")
+
+// title.innerHTML = "<em>Welcome to NYCDA!</em>"
+// helpMessage.innerText = "Ask someone at NYCDA for help!"
+
+// redBorder.style.borderColor = "red"

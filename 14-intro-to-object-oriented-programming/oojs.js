601060d474b88e49bc6fb9c3aaf8654f625f8359
orlandocaraballo <orlando.caraballo@nycda.com>
2018-03-23 08:43:01 -0400

    Added some text in oojs examples

--- 14-intro-to-object-oriented-programming/oojs.js
+++ 14-intro-to-object-oriented-programming/oojs.js
@@ -2,3 +2,5 @@
 let firstNameElement = document.querySelector("#first-name")
 let lastNameElement = document.querySelector("#last-name")
 let ageElement = document.querySelector("#age")
+
+// what code would I write to fill in the values in the html?

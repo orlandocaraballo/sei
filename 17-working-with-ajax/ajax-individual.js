27b0557bee7d1d0a34c915ee96c29f25dbe9bd61
orlandocaraballo <orlando.caraballo@nycda.com>
2018-03-28 15:16:53 -0400

    Added individual json stuff

--- 17-working-with-ajax/ajax-individual.js
+++ 17-working-with-ajax/ajax-individual.js
@@ -1,6 +1,3 @@
-// selecting ul from the html side
-let ul = document.querySelector("ul")
-
 // container class
 class People {
   constructor() {
@@ -22,65 +19,22 @@ class Person {
   }
 }
 
+// selecting ul from the html side
+let ul = document.querySelector("ul")
+let people = new People()
+
 // retrieve data from json
-axios.get("https://raw.githubusercontent.com/orlandocaraballo/sei-march-5/master/17-working-with-ajax/data.json")
+axios.get("https://raw.githubusercontent.com/orlandocaraballo/sei-march-5/master/17-working-with-ajax/person-1.json")
 .then((response) => {
   let data = response.data
-  let people = new People()
+  let person = new Person(
+    data["first-name"],
+    data["last-name"],
+    data["age"],
+    data["gender"]
+  )
 
-  data.forEach((item) => {
-    let li = document.createElement("li")
-
-    people.add(
-      new Person(
-        item["first-name"],
-        item["last-name"], 
-        item["age"], 
-        item["gender"]
-      )
-    )
-
-    li.innerHTML = item["first-name"]
-    ul.appendChild(li)
-  })
-
-  // let people = new People([
-  //   new Person(
-  //     data[0]["first-name"], 
-  //     data[0]["last-name"], 
-  //     data[0]["age"], 
-  //     data[0]["gender"]
-  //   ),
-  //   new Person(
-  //     data[1]["first-name"], 
-  //     data[1]["last-name"], 
-  //     data[1]["age"], 
-  //     data[1]["gender"]
-  //   ),
-  //   new Person(
-  //     data[2]["first-name"], 
-  //     data[2]["last-name"], 
-  //     data[2]["age"], 
-  //     data[2]["gender"]
-  //   )
-  // ])
-
-  // let orlando = new Person(
-  //   data[0]["first-name"], 
-  //   data[0]["last-name"], 
-  //   data[0]["age"], 
-  //   data[0]["gender"]
-  // )
-
-  // people.all.forEach((element) => {
-  //   let li = document.createElement("li")
-  //   li.innerHTML = element.firstName
-  //   ul.appendChild(li)
-
-  //   console.log(element)
-  // })
-
-  console.log(people)
+  people.add(person)  
 }).catch((error) => {
   console.log(error)
 })

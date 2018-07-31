f62f2c6b6bc258af0d1b7b92c7ff8dcbb77bbcdf
orlandocaraballo <orlando.caraballo@nycda.com>
2018-07-31 15:17:28 -0400

    Added exercise 2 and 3

---
+++ outlines/07-intro-to-programming/exercise-2.js
@@ -0,0 +1,15 @@
+// 1
+let classMates = ['kevin', 'janice', 'rodrigo', 'terrance', 'pablo']
+
+// 2
+
+console.log(classMates[3][7])
+
+// 3
+let multidimension = [
+  ["hello", "goodbye", "food"],
+  ["something", "else", "here"],
+  ["beans", "fruit", "veggies"]
+]
+
+console.log(multidimension[2][0])
---
+++ outlines/07-intro-to-programming/exercise-3.js
@@ -0,0 +1,17 @@
+// 1
+
+let girlNextDoor = {
+  name: "mary",
+  eyeColor: "brown",
+  hairColor: "blonde"
+}
+
+// 2
+
+let myFavoriteMovies = {
+  topMovieOne: "pulp fiction",
+  topMovieTwo: "reservoir dogs",
+  topMovieThree: "django unchained",
+  topMovieFour: "four rooms"
+}
+console.log(`My favorite movies are ${ myFavoriteMovies['topMovieOne'] }`)

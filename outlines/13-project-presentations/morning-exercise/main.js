5594938a63da7ad4a96381d2480e3fbc11408439
orlandocaraballo <orlando.caraballo@nycda.com>
2018-08-08 11:48:35 -0400

    Fixed some bugs

--- outlines/13-project-presentations/morning-exercise/main.js
+++ outlines/13-project-presentations/morning-exercise/main.js
@@ -1,18 +1,22 @@
 // captures the p tags into the paragraphs variable
 let paragraphs = document.getElementsByTagName("p")[0]
 
+let divs = document.getElementsByTagName("div")[0]
+
 // stores all anchors stored within a list item that
 //  is within the unordered list
 let anchors = document.querySelectorAll("ul li a")
 
 // makes it so that the click handler is set to the function
-div.addEventListener("click", myScript)
+divs.addEventListener("click", myScript)
 
 // creates a function called myScript that will
 //  be set as the click handler
 function myScript(event) {
   // changes the color to red
   document.querySelector("ul li a").style.color = "red"
+
+  divs.classList.add("large")
   
   // changes all anchor colors to red
   for(let i = 0; i < anchors.length; ++i) {

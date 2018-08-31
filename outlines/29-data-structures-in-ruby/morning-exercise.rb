650db7e67796f11d32d344148b4cade666c9c629
orlandocaraballo <orlando.caraballo@nycda.com>
2018-08-31 14:11:13 -0400

    Added morning exercise solution for carousel

--- outlines/29-data-structures-in-ruby/morning-exercise.rb
+++ outlines/29-data-structures-in-ruby/morning-exercise.rb
@@ -2,12 +2,24 @@ class Array
   def carousel
     while true do
       puts self.to_s
-      element = self.shift
-      self.push(element)
+
+      self.push(self.shift)
+
       sleep 2
     end
   end
 end
 
-([1,2,3,4,5,6]).carousel
+[1,2,3,4,5,6].carousel
 
+# def carousel(array)
+#   while true do
+#     puts array.to_s
+    
+#     array.push(array.shift)
+
+#     sleep 2
+#   end
+# end
+
+# carousel([1,2,3,4,5,6])

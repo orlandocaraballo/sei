a49e0abbb01b2ba9055d617489cdc98054151a70
orlandocaraballo <orlando.caraballo@nycda.com>
2018-09-27 14:59:31 -0400

    Added more class examples

--- outlines/46-ror-views-models/twitter_clone/app/models/application_record.rb
+++ outlines/46-ror-views-models/twitter_clone/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def created_at_formatted
+    self.created_at.strftime('%m/%d/%Y')
+  end
+
+  def updated_at_formatted
+    self.updated_at.strftime('%m/%d/%Y')
+  end
 end
--- outlines/46-ror-views-models/twitter_clone/config/routes.rb
+++ outlines/46-ror-views-models/twitter_clone/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :users do
-    get 'email', to: 'users#email'
-  end
-
+  resources :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

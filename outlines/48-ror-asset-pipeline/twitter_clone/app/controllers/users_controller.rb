426ac8e999f09814faf8918f3d232f3c8a1f0233
orlandocaraballo <orlando.caraballo@nycda.com>
2018-10-03 14:23:30 -0400

    Added twitter clone

---
+++ outlines/48-ror-asset-pipeline/twitter_clone/app/controllers/users_controller.rb
@@ -0,0 +1,109 @@
+class UsersController < ApplicationController
+  layout 'orlando_layout'
+
+  # get '/users' do
+  #   @users = User.all
+  #   erb :index
+  # end
+  def index
+    @users = User.all
+  end
+
+  # get '/users/:id' do
+  #   @users = User.find(params[:id])
+  #   erb :show
+  # end
+  def show
+    # loads the user with id of params[:id]
+    #   from the database
+    @user = User.find(params[:id])
+  end
+
+  # get '/users/:id/edit' do
+  #   @user = User.find(params[:id])
+  #   erb :edit
+  # end
+  def edit
+    # loads the user with id of params[:id]
+    #   from the database
+    @user = User.find(params[:id])
+  end
+
+  # put '/users/:id' do
+  #   user = User.find(params[:id])
+  #   user.update(
+  #     username: params[:user][:username],
+  #     password: params[:user][:password]
+  #   )
+  #
+  #   redirect "/users/#{ params[:id] }"
+  # end
+  def update
+    # loads the user with id of params[:id]
+    #   from the database
+    user = User.find(params[:id])
+
+    # updates the user that was currently loaded
+    #   with the information retrieved from the
+    #   form
+    user.update(
+      username: params[:user][:username],
+      password: params[:user][:password]
+    )
+
+    # goes to show page
+    redirect_to user_path(user)
+  end
+
+  # delete '/users/:id' do
+  #   user = User.find(params[:id])
+  #
+  #   user.destroy
+  #
+  #   redirect "/users"
+  # end
+  def destroy
+    # this loads the user first
+    user = User.find(params[:id])
+
+    # this uses the information inside the user
+    #   object and deletes the user
+    user.destroy
+    # User.destroy(params[:id])
+
+    redirect_to users_path
+  end
+
+  # get '/users/new' do
+  #   @user = User.new
+  #   erb :new
+  # end
+  def new
+    # creates a new instance of type User
+    @user = User.new
+  end
+
+  # post '/users' do
+  #   user = User.create(
+  #     username: params[:user][:username],
+  #     password: params[:user][:password]
+  #   )
+  #
+  #   session[:user_id] = user.id
+  # 
+  #   redirect '/users'
+  # end
+  def create
+    # creates a new user
+    user = User.create(
+      username: params[:user][:username],
+      password: params[:user][:password]
+    )
+
+    # signs the newly created user in
+    session[:user_id] = user.id
+
+    # redirects to the '/users' path
+    redirect_to users_path
+  end
+end

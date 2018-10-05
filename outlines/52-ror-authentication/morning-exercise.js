c0a2a7e5e3dc792a30b4e7618e1645fe4e170bf6
orlandocaraballo <orlando.caraballo@nycda.com>
2018-10-05 14:24:08 -0400

    git st

---
+++ outlines/52-ror-authentication/morning-exercise.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function studentFactory(course) {
+  return (name, gender, age) => {
+    return { 
+      name: name, 
+      gender: gender, 
+      age: age, 
+      course: course,
+      instructor: 'orlando'
+    };
+  }
+}
+
+let generateSeiStudent = studentFactory("sei");
+console.log(generateSeiStudent('orlando', 'male', 23));

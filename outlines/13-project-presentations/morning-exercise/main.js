8906ea78a41a71f862a9ded83c2332a51cf26af1
orlandocaraballo <orlando.caraballo@nycda.com>
2018-11-30 10:01:25 -0500

    Removed unnecessary examples for morning exercise

--- outlines/13-project-presentations/morning-exercise/main.js
+++ outlines/13-project-presentations/morning-exercise/main.js
@@ -1,11 +1,5 @@
-// captures the p tags into the paragraphs variable
-let paragraphs = document.getElementsByTagName("p")[0]
-
+// captures the div tags into the divs variable
 let divs = document.getElementsByTagName("div")[0]
-
-// stores all anchors stored within a list item that
-//  is within the unordered list
-let anchors = document.querySelectorAll("ul li a")
 
 // makes it so that the click handler is set to the function
 divs.addEventListener("click", myScript)
@@ -13,15 +7,5 @@ divs.addEventListener("click", myScript)
 // creates a function called myScript that will
 //  be set as the click handler
 function myScript(event) {
-  // changes the color to red
-  document.querySelector("ul li a").style.color = "red"
-
   divs.classList.add("large")
-  
-  // changes all anchor colors to red
-  for(let i = 0; i < anchors.length; ++i) {
-    // targets one individual anchor and
-    //  changes the color to red
-    anchors[i].style.color = "red"
-  }
 }

cdbe0c90701c039dc4eb9ff8ca842a8c34b2846d
orlandocaraballo <orlando.caraballo@nycda.com>
2018-12-05 09:02:17 -0500

    Added comments to workshop

--- outlines/16-pokedex-reveal/index.js
+++ outlines/16-pokedex-reveal/index.js
@@ -1,26 +1,70 @@
-//This comes from data.js
-let gifs = window.data.data;
-let main = document.querySelector("main");
-let h1 = document.querySelector("h1");
-let image = document.createElement("img");
-let imageUrlArray = generateUrlArray(gifs);
+// gifs is set to data from data.js
+const gifs = window.data.data;
+const main = document.querySelector("main");
+const h1 = document.querySelector("h1");
+const image = document.createElement("img");
+const imageUrlArray = generateUrlArrayUsingMap(gifs);
 
+// set the click handler of the h1
 h1.addEventListener("click", event => {
-  let index = randomInteger(imageUrlArray.length);
-  let randomImageUrl = imageUrlArray[index];
+  // pick a random number between 0 and
+  //  the current length
+  const index = randomInteger(imageUrlArray.length);
+
+  // get the gif url stored in imageUrlArray
+  const randomImageUrl = imageUrlArray[index];
   
+  // change the src attribute to gif url
   image.setAttribute("src", randomImageUrl);
+
+  // append image to main element
   main.appendChild(image);
 
+  // remove the element at position index
+  //  has the side effect of reducing array size by 1
   imageUrlArray.splice(index, 1);
 })
 
+// pick a random integer
 function randomInteger(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+// generate new array
 function generateUrlArray(gifs) {
-  return gifs.map(element => {
+  // create empty array
+  const gifUrls = [];
+
+  // loop thru gifs array, get the origin url image
+  //  push them into gifUrls
+  for(let i = 0; i < gifs.length; ++i) {
+    gifUrls.push(gifs[i].images.original.url);
+  }
+
+  return gifUrls;
+}
+
+// using forEach to loop
+function generateUrlArrayUsingForEach(gifs) {
+  // create empty array
+  const gifUrls = [];
+
+  // loop thru gifs array, get the origin url image
+  //  push them into gifUrls
+  gifs.forEach(element => {
+    gifUrls.push(element.images.original.url);
+  })
+
+  return gifUrls;
+}
+
+// using map to generate new array
+function generateUrlArrayUsingMap(gifs) {
+  // loop thru gifs array, get the origin url image
+  //  copy them into gifUrls
+  const gifUrls = gifs.map(element => {
     return element.images.original.url;
   })
+
+  return gifUrls;
 }

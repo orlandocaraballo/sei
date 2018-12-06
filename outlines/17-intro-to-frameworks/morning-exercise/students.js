0797dbbb622dd07f0b080197b4b89b4efac49475
orlandocaraballo <orlando.caraballo@nycda.com>
2018-12-06 11:02:31 -0500

    Added solution for today's workshop

--- outlines/17-intro-to-frameworks/morning-exercise/students.js
+++ outlines/17-intro-to-frameworks/morning-exercise/students.js
@@ -1,6 +1,9 @@
+"use strict";
+
+// set our students data array
 const students = [
   {
-    name: "mohammed",
+    name: "mohamed",
     gender: "male",
     famousPhrase: "[Insert question about topic already explained here]"
   },
@@ -21,4 +24,32 @@ const students = [
   }
 ]
 
+// store ul within ul variable
+const ul = document.getElementsByTagName("ul")[0];
+
 // your code goes here
+for(let i = 0; i < students.length; ++i) {
+  let name = students[i].name;
+  let gender = students[i].gender;
+  let famousPhrase = students[i].famousPhrase;
+
+  let li = document.createElement("li");
+  li.innerHTML += `<strong>name: ${ name }</strong><br />`;
+  li.innerHTML += `<em>gender: ${ gender }</em><br />`;
+  li.innerHTML += `<p>famous phrase: ${ famousPhrase }</p>`;
+  ul.appendChild(li);
+}
+
+// ------ INTRODUCT OF FOREACH ----
+const array = [1,2,3,4];
+
+// this code and the code on line 50
+//  are equivalent
+for(let i = 0; i < array.length; ++i) {
+  console.log(array[i]);
+}
+
+// this is the same as the code on line 47
+array.forEach(element => {
+  console.log(element);
+});

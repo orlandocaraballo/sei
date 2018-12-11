8a8ff0f576ca55f14cab09977acb96f3ffd09208
orlandocaraballo <orlando.caraballo@nycda.com>
2018-12-11 12:31:49 -0500

    Added code written in class

--- outlines/20-introduction-to-apis/simple-example/simple-example.js
+++ outlines/20-introduction-to-apis/simple-example/simple-example.js
@@ -1,6 +1,31 @@
 "use strict";
 
-axios.get("https://sei-api.herokuapp.com/students/1")
+class Student {
+  constructor(name, gender, knownFor) {
+    this.name = name;
+    this.gender = gender;
+    this.knownFor = knownFor;
+  }
+}
+
+axios.get("https://sei-api.herokuapp.com/students")
 .then(response => {
-  console.log(response.data);
+  // console.log(response.data);
+  let data = response.data;
+
+  for(let i = 0; i < data.length; ++i) {
+    let student = new Student(
+      data[i].name,
+      data[i].gender,
+      data[i].knownFor
+    );
+
+    console.log(student);
+
+    // document.body.innerHTML += "<p>";
+    document.body.innerHTML += `<p><strong>name = ${ data[i].name}</strong>`;
+    document.body.innerHTML += `<em>gender = ${ data[i].gender}</em></p>`;
+    // document.body.innerHTML += "</p>";
+
+  }
 })

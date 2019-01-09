3f14b1997660b2f8888527c2f785d298919b3fd0
orlandocaraballo <orlando.caraballo@nycda.com>
2019-01-09 17:14:31 -0500

    Added sass stuff

--- outlines/34-views-layouts-sinatra/example/app.rb
+++ outlines/34-views-layouts-sinatra/example/app.rb
@@ -20,6 +20,17 @@ get "/groceries/:id" do
   erb(:grocery)
 end
 
+get "/form" do
+  erb(:form)
+end
+
+post "/person" do
+  puts "-----THE PARAMETERS ARE -> #{params} ----------------"
+
+  # in here you can use these params to sign someone up or log them in
+  #   or a variety of other things
+end
+
 get "/main.css" do
   scss(:main)
 end

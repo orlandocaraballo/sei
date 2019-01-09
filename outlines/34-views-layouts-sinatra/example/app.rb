d6bab2fe11b5a4aa9334baa3f38cbf0b4427248a
orlandocaraballo <orlando.caraballo@nycda.com>
2019-01-09 11:51:21 -0500

    Added examples

--- outlines/34-views-layouts-sinatra/example/app.rb
+++ outlines/34-views-layouts-sinatra/example/app.rb
@@ -1,17 +1,21 @@
 require "sinatra"
 
-{
-  name: "orlando",
-  locals: 
-}
+groceries = %w{cereal bread milk eggs }
 
 get "/" do
-  groceries = %w{cereal bread milk eggs }
+  erb(:home)
+end
 
-  erb(:index, {
-    locals: { 
-      groceries: groceries,
-      name: "aron"
-    }
-  })
+get "/groceries" do
+  @groceries = groceries
+
+  erb(:groceries)
+end
+
+get "/groceries/:id" do
+  index = params[:id].to_i
+
+  @food_item = groceries[index - 1]
+
+  erb(:grocery)
 end
